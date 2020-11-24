1a2fbfdd1f20b408b64c9309a9bfd2a6652b36e4
Lettera <lorenzocav97@gmail.com>
2020-11-25 00:50:03 +0100

    fixed the slice bug

---
+++ .ipynb_checkpoints/audio-checkpoint.py
@@ -0,0 +1,40 @@
+import json
+from shutil import copy2
+import os
+
+identifier=4727634165
+
+with open("result.json","r") as read_file:
+    data=json.load(read_file)
+
+#Given the name or the id it returs the otherone
+def find(x):
+    for contact in data['frequent_contacts']['list']:
+        if contact['id']==x: return contact['name']
+        if contact['name']==x: return contact['id']
+
+#Return a list of the path of the files of the audios sent
+def extract_audio(identifier,name,json):
+    files=[]
+    for chat in data['chats']['list']:
+        for message in chat['messages']:
+            if ("media_type" in message) and message['from_id']==identifier and message["media_type"]=="voice_message":
+                files.append(message["file"])
+        if chat['type']=='private_group':
+            for membri in chat['messages'][0]['members']:
+                if membri==name:
+                    for message in chat['messages']:
+                        if ("media_type" in message) and message['from']==name and message["media_type"]=="voice_message":
+                            files.append(message["file"])
+                        
+    return files           
+
+name=find(identifier)
+files=extract_audio(identifier,name,"result.json")
+
+
+newpath = 'new_folder' 
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+for file in files:
+    copy2(file,'new_folder/')
